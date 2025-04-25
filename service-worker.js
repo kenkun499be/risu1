473c7d9660a52fec16898cfbcd371f9fa4eb5419
kenkun499be <kenkun499be@outlook.jp>
2025-04-25 14:49:07 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,7 @@
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open("v1").then(function (cache) {
-      return cache.addAll([
-        "/",
-        "/index.php",
-        "/css/style.css",
-        "/js/app.js",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png"
-      ]);
-    })
-  );
+self.addEventListener('install', (event) => {
+  self.skipWaiting();  // 新しいservice workerをすぐに適用
 });
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());  // 新しいservice workerがすぐにアクティブになる
 });
